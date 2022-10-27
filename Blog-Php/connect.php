434e6b98c0e1b60a86a540dfbed14abdff4e39ec
Krebs jérémie <72922751+suncher@users.noreply.github.com>
2022-10-27 15:22:42 +0200

    Update connect.php

--- Blog-Php/connect.php
+++ Blog-Php/connect.php
@@ -1,6 +1,6 @@
 <?php
     try { 
-        $objPdo = new PDO ('mysql:host=devbdd.iutmetz.univ-lorraine.fr;port=3306:charset=utf8; dbname=krebs47u_php' , 'krebs47u_appli' , 'xxx' );  
+        $objPdo = new PDO ('mysql:host=devbdd.iutmetz.univ-lorraine.fr;port=3306:charset=utf8; dbname=krebs47u_php' , 'id' , 'password' );  
         
     }  catch( Exception $exception ) {  
         die($exception->getMessage()); 
